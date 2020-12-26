7955bb9802942d9bae0d992323a33da6174cf7e6
Piotr Opyd <opydpiotrek@gmail.com>
2020-12-26 21:34:08 +0100

    update comments

--- git_repository_analyzer/network/APIClient.py
+++ git_repository_analyzer/network/APIClient.py
@@ -1,18 +1,22 @@
 # TODO: bootstrap script to install dependencies
 import requests
 
-# To fetch data about repository call APIClient.{method_name(id)} eg. APIClient.get_gitlab_project(6853087)
+# To fetch repository data call APIClient.{method_name(id)} 
+# eg. APIClient.get_gitlab_project(6853087)
+
 class APIClient:
 
     __github_api_base_url = 'https://api.github.com/'
     __gitlab_api_base_url = 'https://gitlab.com/api/v4/'
 
+    # Returns data about any public GitHub repository with given id in JSON format
     @classmethod
     def get_github_project(cls, id):
         url = f'{cls.__github_api_base_url}projects/{id}'
         response = requests.get(url)
-        return response
+        return response.json()
     
+    # Returns data about any public GitLab repository with given id in JSON format
     @classmethod
     def get_gitlab_project(cls, id):
         url = f'{cls.__gitlab_api_base_url}projects/{id}'
